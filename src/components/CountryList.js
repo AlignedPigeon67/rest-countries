647c0902e356011c22293622ccb6909904fcc19b
tomhine792 <tomhine792@outlook.com>
2021-07-23 23:41:35 +0100

    add filtering and searching logic

--- src/components/CountryList.js
+++ src/components/CountryList.js
@@ -1,5 +1,5 @@
-import styled from "styled-components";
-import CountryItem from "./CountryItem";
+import styled from 'styled-components';
+import CountryItem from './CountryItem';
 
 const List = styled.section`
   display: flex;
@@ -8,11 +8,20 @@ const List = styled.section`
   align-items: center;
 `;
 
-const CountryList = ({ allCountries }) => {
+const CountryList = ({ allCountries, query, filter }) => {
+  const filteredCountries = filter
+    ? allCountries.filter(country => country.region.toLowerCase() === filter)
+    : allCountries;
+  const searchedCountries = query
+    ? filteredCountries.filter(country =>
+        country.name.toLowerCase().includes(query)
+      )
+    : filteredCountries;
+
   return (
     <List>
-      {allCountries.map((country) => (
-        <CountryItem country={country} />
+      {searchedCountries.map(country => (
+        <CountryItem country={country} key={country.alpha3Code} />
       ))}
     </List>
   );
