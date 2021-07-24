1d3e6dbaf2e60c3a8fabd5dbb6843d77bcb22063
tomhine792 <tomhine792@outlook.com>
2021-07-24 19:19:22 +0100

    add shadow color to themes

--- src/ui/themes.js
+++ src/ui/themes.js
@@ -1,15 +1,17 @@
 const themes = {
   dark: {
-    background: "hsl(207, 26%, 17%)",
-    text: "hsl(0, 0%, 100%)",
-    element: "hsl(209, 23%, 22%)",
-    input: "hsl(0, 0%, 100%)",
+    background: 'hsl(207, 26%, 17%)',
+    text: 'hsl(0, 0%, 100%)',
+    element: 'hsl(209, 23%, 22%)',
+    input: 'hsl(0, 0%, 100%)',
+    shadow: 'rgba(0, 0, 0, 0.1)',
   },
   light: {
-    background: "hsl(0, 0%, 98%)",
-    text: "hsl(200, 15%, 8%)",
-    element: "hsl(0, 0%, 100%)",
-    input: "hsl(200, 15%, 8%)",
+    background: 'hsl(0, 0%, 98%)',
+    text: 'hsl(200, 15%, 8%)',
+    element: 'hsl(0, 0%, 100%)',
+    input: 'hsl(200, 15%, 8%)',
+    shadow: 'rgba(0, 0, 0, 0.03)',
   },
 };
 
