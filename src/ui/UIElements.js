37634514df38623d64a7598d3dfb9dbd97a6bc1a
tomhine792 <tomhine792@outlook.com>
2021-07-24 19:06:09 +0100

    add button link element and added gap to main grid

--- src/ui/UIElements.js
+++ src/ui/UIElements.js
@@ -14,5 +14,21 @@ export const Unlink = styled(Link)`
 export const Main = styled.main`
   display: flex;
   flex-direction: column;
+  gap: 3.5rem;
   padding: 2rem 2rem;
+`;
+
+export const Button = styled(Link)`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 10.5rem;
+  height: 3.5rem;
+  padding: 0 2rem;
+  border-radius: 0.3rem;
+  background-color: ${({ theme }) => theme.element};
+  color: ${({ theme }) => theme.text};
+  text-decoration: none;
+  font-size: 1.4rem;
+  box-shadow: 0 0 5px 5px ${({ theme }) => theme.shadow};
 `;
