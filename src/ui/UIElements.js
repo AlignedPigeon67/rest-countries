ac69d0382a652d4e881892b4d8e416e71abfe25d
tomhine792 <tomhine792@outlook.com>
2021-07-25 09:10:17 +0100

    refine margin and padding of main container

--- src/ui/UIElements.js
+++ src/ui/UIElements.js
@@ -15,7 +15,8 @@ export const Main = styled.main`
   display: flex;
   flex-direction: column;
   gap: 3.5rem;
-  padding: 2rem 2rem;
+  padding: 3rem 2rem;
+  margin-bottom: 2rem;
 `;
 
 export const Button = styled(Link)`
