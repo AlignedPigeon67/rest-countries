028f1146cf9531e8f6c1855d2fe30dadeaf92876
tomhine792 <tomhine792@outlook.com>
2021-07-25 09:15:12 +0100

    add missing semi-colon

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Details from './pages/Details';
 
 const GlobalStyle = createGlobalStyle`
   body {
-    background-color: ${({ theme }) => theme.element}
+    background-color: ${({ theme }) => theme.element};
   }
 `;
 
