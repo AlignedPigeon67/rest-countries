d0f2fe08059261f7dfe05bfe9c605031e1d451f5
Tom Hine <tomhine792@gmail.com>
2021-08-09 14:16:57 +0100

    add media query to extend width on larger screens

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -5,13 +5,17 @@ const Container = styled.div`
   display: flex;
   align-items: center;
   gap: 2.2rem;
-  width: 34rem;
-  height: 4.5rem;
+  width: 30rem;
+  height: 5rem;
   background-color: ${({ theme }) => theme.element};
   color: ${({ theme }) => theme.text};
   border-radius: 0.5rem;
   padding: 0 2.6rem;
   box-shadow: 0 0 5px 5px ${({ theme }) => theme.shadow};
+
+  @media (min-width: 700px) {
+    width: 40%;
+  }
 `;
 
 const StyledSearchIcon = styled(SearchIcon)`
